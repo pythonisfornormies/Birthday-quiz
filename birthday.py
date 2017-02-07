bc640a6436a2e8f161066b65aea5049a8a610c04
pythonisfornormies <kaidarrow@hanovernorwichschools.org>
2017-02-07 14:20:57 -0500

    Updated from Brython Server: 2/7/2017 2:20:56 PM

--- birthday.py
+++ birthday.py
@@ -1,7 +1,7 @@
 """
 birthday.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Kai Darrow
+Credit: Me Me I Me Me
 Assignment:
 
 Your program will ask the user the following questions, in this order:
